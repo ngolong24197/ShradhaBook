e66893166dbb23d621cdcb17e04ba11bd81507d5
admin <admin@DESKTOP-TVGKKG4>
2024-03-08 21:01:32 +0700

    Add project files.

---
+++ ShradhaBook/Program.cs
@@ -0,0 +1,81 @@
+using ShradhaBook.DataAccess.Repository;
+using ShradhaBook.DataAccess.Repository.IRepository;
+using ShradhaBook.DataAcess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using ShradhaBook.Utility;
+using Stripe;
+using ShradhaBook.DataAccess.DbInitializer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options=> 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+builder.Services.AddAuthentication().AddFacebook(option => {
+    option.AppId = "1316607505684274";
+    option.AppSecret = "35d89e3151cccfdfedde8b1a2ab8256c";
+});
+builder.Services.AddAuthentication().AddMicrosoftAccount(option => {
+    option.ClientId = "BiG8Q~TyzBOiGdM-VAUpcubOUSvn9QKKAdXZfayE";
+    option.ClientSecret = "65b16f00-51a8-4d96-ac10-e83387753b56";
+});
+builder.Services.AddAuthentication().AddGoogle(option => {
+    option.ClientId= "265635693875-vvs32riilgsr8livrkjurcm5e8je0u0k.apps.googleusercontent.com";
+    option.ClientSecret = "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";
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+SeedDatabase();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+void SeedDatabase() {
+    using (var scope = app.Services.CreateScope()) {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
